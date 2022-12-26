472cd596599da17d0eb1845fe82a5c16f06fb09e
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-26 16:18:33 +0600

    search functionality design done

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,21 @@ const Header = () => {
   ]);
 
   const [openDate, setOpenDate] = useState(false);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+  const handleOption = (name, operations) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operations === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
 
   return (
     <div className="px-6 bg-[#40415a]  text-white ">
@@ -89,9 +104,79 @@ const Header = () => {
         </div>
         <div className="flex items-center gap-5 m-3 ">
           <FontAwesomeIcon icon={faPerson} className="text-gray-500" />
-          <span className="text-gray-500  cursor-pointer ">
-            2 adults 2 clidren 1 room
+          <span
+            className="text-gray-500  cursor-pointer "
+            onClick={() => setOpenOptions(!openOptions)}
+          >
+            {`${options.adult} Adult. ${options.children} Children. ${options.room} Room`}
           </span>
+          {openOptions && (
+            // <span className="absolute top-36 md:top-16 bg-red-800">
+            //   Options
+            // </span>
+
+            <div className="absolute top-36 md:top-16 bg-white text-gray-500  drop-shadow-2xl">
+              <div className="w-[300px] flex justify-between m-3">
+                <span className="optionstext">Adult</span>
+                <div className="flex items-center gap-3">
+                  <button
+                    className="btn btn-square btn-xs"
+                    onClick={() => handleOption("adult", "d")}
+                    disabled={options.adult <= 1}
+                  >
+                    -
+                  </button>
+                  <span className="optioncounternumber">{options.adult}</span>
+                  <button
+                    className="btn btn-square btn-xs"
+                    onClick={() => handleOption("adult", "i")}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="w-[300px] flex justify-between m-3">
+                <span className="optionstext">Children</span>
+                <div className="flex items-center gap-3">
+                  <button
+                    className="btn btn-square btn-xs"
+                    onClick={() => handleOption("children", "d")}
+                    disabled={options.children <= 0}
+                  >
+                    -
+                  </button>
+                  <span className="optioncounternumber">
+                    {options.children}{" "}
+                  </span>
+                  <button
+                    className="btn btn-square btn-xs"
+                    onClick={() => handleOption("children", "i")}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="w-[300px] flex justify-between m-3">
+                <span className="optionstext">Room</span>
+                <div className="flex items-center gap-3">
+                  <button
+                    className="btn btn-square btn-xs"
+                    disabled={options.room <= 1}
+                    onClick={() => handleOption("room", "d")}
+                  >
+                    -
+                  </button>
+                  <span className="optioncounternumber">{options.room}</span>
+                  <button
+                    className="btn btn-square btn-xs"
+                    onClick={() => handleOption("room", "i")}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
         <div className="flex items-center justify-start md:justify-center ml-3 gap-5  ">
           <button className="btn btn-primary w-3/6 ">Search</button>
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
 
 const List = () => {
   return (
     <div>
       <Navbar />
+      <Header type="list" />
       <p>This is list page</p>
     </div>
   );
