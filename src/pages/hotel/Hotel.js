b2ab35567343e12b955da5e2be73decdb29a9c28
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-29 00:56:12 +0600

    failed to fix slider

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -4,17 +4,9 @@ import Header from "../../components/Header";
 import MailList from "../../components/MailList";
 import Footer from "../../components/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleArrowLeft,
-  faCircleArrowRight,
-  faCircleXmark,
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 const Hotel = () => {
-  const [slideNumber, setSlideNumber] = useState(0);
-  const [open, setOpen] = useState(false);
-
   const photos = [
     {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
@@ -36,55 +28,12 @@ const Hotel = () => {
     },
   ];
 
-  const handleOpen = (i) => {
-    setSlideNumber(i);
-    setOpen(true);
-  };
-
-  const handleMove = (direction) => {
-    let newSlideNumber;
-
-    if (direction === "l") {
-      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
-    } else {
-      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
-    }
-
-    setSlideNumber(newSlideNumber);
-  };
-
   return (
     <div>
       <div>
         <Navbar />
         <Header type="list" />
         <div className="flex justify-center mt-6">
-          {open && (
-            <div className="slider">
-              <FontAwesomeIcon
-                icon={faCircleXmark}
-                className="close"
-                onClick={() => setOpen(false)}
-              />
-              <FontAwesomeIcon
-                icon={faCircleArrowLeft}
-                className="arrow"
-                onClick={() => handleMove("l")}
-              />
-              <div className="sliderWrapper">
-                <img
-                  src={photos[slideNumber].src}
-                  alt=""
-                  className="sliderImg"
-                />
-              </div>
-              <FontAwesomeIcon
-                icon={faCircleArrowRight}
-                className="arrow"
-                onClick={() => handleMove("r")}
-              />
-            </div>
-          )}
           <div className="w-full max-w-5xl flex flex-col gap-2 relative ">
             <button className="absolute top-3 right-0 bg-[#1f214d] p-1 text-white outline-none font-medium rounded">
               Reserve or Book Now!
@@ -103,12 +52,7 @@ const Hotel = () => {
             <div className="flex flex-wrap justify-between">
               {photos.map((photo, i) => (
                 <div className="w-[33%] " key={i}>
-                  <img
-                    onClick={() => handleOpen(i)}
-                    src={photo.src}
-                    alt=""
-                    className="w-full object-cover"
-                  />
+                  <img src={photo.src} alt="" className="w-full object-cover" />
                 </div>
               ))}
             </div>
