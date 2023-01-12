7a52a1c0622c55b7945ea9f0566737f800429341
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2023-01-12 14:55:19 +0600

    getting cities and properties data from backend

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,43 +1,54 @@
 import React from "react";
+import useFetch from "../hookes/useFetch";
 
 const Featured = () => {
+  const { data, loading, error, reFetchData } = useFetch(
+    "http://localhost:5000/server/hotels/countByCity?cities=Dhaka,Rangamati,Sylhet"
+  );
+  // console.log(data);
   return (
     <>
       <div className="w-full max-w-5xl	grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-2">
-        <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-            alt=""
-            className="featuredImg"
-          />
-          <div className="absolute bottom-[30px] p-2">
-            <h1 className="text-xl font-bold">Rangamati</h1>
-            <h2>123 properties</h2>
-          </div>
-        </div>
+        {loading ? (
+          "Loading wait ..."
+        ) : (
+          <>
+            <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="absolute bottom-[30px] p-2">
+                <h1 className="text-xl font-bold">Dhaka</h1>
+                <h2>{data[0]} properties</h2>
+              </div>
+            </div>
 
-        <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-            alt=""
-            className="featuredImg"
-          />
-          <div className="absolute bottom-[30px] p-2">
-            <h1 className="text-xl font-bold">Dhaka</h1>
-            <h2>533 properties</h2>
-          </div>
-        </div>
-        <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
-          <img
-            src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-            alt=""
-            className="featuredImg"
-          />
-          <div className="absolute bottom-[30px] p-2">
-            <h1 className="text-xl font-bold">Sylhet</h1>
-            <h2>532 properties</h2>
-          </div>
-        </div>
+            <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="absolute bottom-[30px] p-2">
+                <h1 className="text-xl font-bold">Rangamati</h1>
+                <h2>{data[1]} properties</h2>
+              </div>
+            </div>
+            <div className="relative text-white rounded-lg overflow-hidden	h-[250px]">
+              <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+                alt=""
+                className="featuredImg"
+              />
+              <div className="absolute bottom-[30px] p-2">
+                <h1 className="text-xl font-bold">Sylhet</h1>
+                <h2>{data[2]} properties</h2>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -1,63 +1,44 @@
 import React from "react";
+import useFetch from "../hookes/useFetch";
 
 const PropertyList = () => {
+  const { data, loading, error, reFetchData } = useFetch(
+    "http://localhost:5000/server/hotels/countByType"
+  );
+  // console.log(data);
+
+  const images = [
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+
   return (
     <div className="w-full max-w-5xl	grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-2 mt-3 ">
-      <div className="rounded-lg overflow-hidden	cursor-pointer	">
-        <img
-          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
-          alt=""
-          className="w-full h-[150px] object-cover	"
-        />
-        <div className="">
-          <h1 className=" font-bold">Hotels</h1>
-          <h2 className="text-sm text-gray-500">233 hotels</h2>
-        </div>
-      </div>
-      <div className="rounded-lg overflow-hidden	cursor-pointer	">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
-          alt=""
-          className="w-full h-[150px] object-cover"
-        />
-        <div className="">
-          <h1 className=" font-bold">Apartments</h1>
-          <h2 className="text-sm text-gray-500">2331 hotels</h2>
-        </div>
-      </div>
-      <div className="rounded-lg overflow-hidden	cursor-pointer	">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
-          alt=""
-          className="w-full h-[150px] object-cover"
-        />
-        <div>
-          <h1 className=" font-bold">Resorts</h1>
-          <h2 className="text-sm text-gray-500">2331 hotels</h2>
-        </div>
-      </div>
-      <div className="rounded-lg overflow-hidden	cursor-pointer	">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
-          alt=""
-          className="w-full h-[150px] object-cover"
-        />
-        <div>
-          <h1 className=" font-bold">Villas</h1>
-          <h2 className="text-sm text-gray-500">2331 hotels</h2>
-        </div>
-      </div>
-      <div className="rounded-lg overflow-hidden	cursor-pointer	">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
-          alt=""
-          className="w-full h-[150px] object-cover"
-        />
-        <div>
-          <h1 className=" font-bold">Cabins</h1>
-          <h2 className="text-sm text-gray-500">2331 hotels</h2>
-        </div>
-      </div>
+      {loading ? (
+        "Loading wait ..."
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="rounded-lg overflow-hidden	cursor-pointer	" key={i}>
+                <img
+                  src={img}
+                  alt=""
+                  className="w-full h-[150px] object-cover	"
+                />
+                <div>
+                  <h1 className="font-bold capitalize">{data[i]?.type}</h1>
+                  <h2 className="text-sm text-gray-500">
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 };
